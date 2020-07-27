7f33d8ad5873ceeada418af38cf086685e13b614
NeroYuki <kingrfminecraft@gmail.com>
2020-07-27 21:35:18 +0700

    27-07-2020: add prefix lol

--- elainaRebuild-event/message.js
+++ elainaRebuild-event/message.js
@@ -4,8 +4,10 @@ const log = require('../elainaRebuild-utils/log.js')
 module.exports = {
     name: 'message',
     execute(client, message) {
+        if (!message.content.startsWith(mainConfig.prefix)) return;
         const [cmd, ...args] = message.content.trim().slice(mainConfig.prefix.length).split(/\s+/g);
         //console.log(args)
+        
         const command = client.commands.get(cmd)
         if (command) {
             command.run(client, message, args)
